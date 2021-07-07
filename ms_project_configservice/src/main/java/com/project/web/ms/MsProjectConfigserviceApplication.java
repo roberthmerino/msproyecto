86166bdb6b05c1e52414a42134220abf88eb5bcd
roberthmerino <47578405+roberthmerino@users.noreply.github.com>
2021-07-07 15:54:36 -0500

    Archivos Configuracion

---
+++ ms_project_configservice/src/main/java/com/project/web/ms/MsProjectConfigserviceApplication.java
@@ -0,0 +1,15 @@
+package com.project.web.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class MsProjectConfigserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProjectConfigserviceApplication.class, args);
+	}
+
+}
---
+++ ms_project_configservice/src/test/java/com/project/web/ms/MsProjectConfigserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.web.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProjectConfigserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
